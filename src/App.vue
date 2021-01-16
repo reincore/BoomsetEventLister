0692a84708026d078a2a5288d026dc4fb0e1734a
Deniz Saglam <dnzsaglam@gmail.com>
2021-01-16 20:15:35 +0300

    Initialized project with Vue CLI, updated Readme and changed initial logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/Boomset Logo.png">
+    <EventDashboard msg="All events:"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EventDashboard from './components/EventDashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EventDashboard
   }
 }
 </script>
--- src/components/EventDashboard.vue
+++ src/components/EventDashboard.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'EventDashboard',
   props: {
     msg: String
   }
